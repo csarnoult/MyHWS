b344f5a69bb989c454c1b0813fa87535eb08c963
csarnoult <chris.arnoult@icloud.com>
2019-11-28 17:24:16 -0800

    Finished coding a check-splitting app

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,21 +8,51 @@
 
 import SwiftUI
 
-
 struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    
     var body: some View {
         NavigationView {
             Form {
                 Section {
-                    Text("Hello, World!")
-                    Text("Hello, World!")
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
                 }
+                
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) {
+                            Text("\(self.tipPercentages[$0])%")
+                        }
+                    }.pickerStyle(SegmentedPickerStyle())
+                }
+                
                 Section {
-                    Text("Hello, World!")
-                    Text("Hello, World!")
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
                 }
+                .navigationBarTitle("WeSplit")
             }
-            .navigationBarTitle(Text("SwiftUI"))
         }
     }
 }
@@ -30,5 +60,5 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-    }
+   }
 }
